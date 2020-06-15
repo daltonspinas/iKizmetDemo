e968d9569bb10a67e2779e98594fd7ad0ae67532
unknown <daltonspinas@gmail.com>
2020-06-14 23:38:33 -0700

    adds ability to export data as a spreadsheet

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,91 +1,111 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService, IChartData, IStats, ITimeRange } from 'src/app/services/data.service';
+import * as XLSX from 'xlsx';
 import * as moment from 'moment';
 
 @Component({
-  selector: 'app-dashboard',
-  templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+    selector: 'app-dashboard',
+    templateUrl: './dashboard.component.html',
+    styleUrls: ['./dashboard.component.scss']
 })
 
 export class DashboardComponent implements OnInit {
 
-  chartData: IChartData;
+    chartData: IChartData;
 
-  data: any[];
-  displayData: any[];
+    data: any[];
+    displayData: any[];
 
-  columns: string[];
+    columns: string[];
 
-  currency: string;
+    currency: string;
 
-  stats: IStats;
+    stats: IStats;
 
-  timeRanges: ITimeRange[] = [
-  {display: 'Last 30 Days', start: moment().subtract(30, 'days').toISOString(), end: moment().toISOString()}, 
-  {display: 'YTD', start: moment().startOf('year').toString(), end: moment().toString()},
-  // TODO dynamically get the current date and determine how many of the current year's quarters to display
-  {display: `Q2 ${moment().year().toString()}`, start: this.getStartOfMonth(3), end: this.getEndOfMonth(5)},
-  {display: `Q1 ${moment().year().toString()}`, start: this.getStartOfMonth(0), end: this.getEndOfMonth(2)},
-  {display: `Q4 ${moment().subtract(1, 'year').year().toString()}`, start: this.getStartOfMonth(9, true), end: this.getEndOfMonth(11, true)},
-  {display: `Q3 ${moment().subtract(1, 'year').year().toString()}`, start: this.getStartOfMonth(6, true), end: this.getEndOfMonth(8,true)},
-  {display: `Q2 ${moment().subtract(1, 'year').year().toString()}`, start: this.getStartOfMonth(3, true), end: this.getEndOfMonth(5,true)},
-  {display: `Q1 ${moment().subtract(1, 'year').year().toString()}`, start: this.getStartOfMonth(0, true), end: this.getEndOfMonth(2,true)}]
+    intervalToGroupBy: string = 'Day'
+
+    timeRanges: ITimeRange[] = [
+        { display: 'Last 30 Days', start: moment().subtract(30, 'days').toISOString(), end: moment().toISOString() },
+        { display: 'YTD', start: moment().startOf('year').toString(), end: moment().toString() },
+        // TODO dynamically get the current date and determine how many of the current year's quarters to display
+        { display: `Q2 ${moment().year().toString()}`, start: this.getStartOfMonth(3), end: this.getEndOfMonth(5) },
+        { display: `Q1 ${moment().year().toString()}`, start: this.getStartOfMonth(0), end: this.getEndOfMonth(2) },
+        { display: `Q4 ${moment().subtract(1, 'year').year().toString()}`, start: this.getStartOfMonth(9, true), end: this.getEndOfMonth(11, true) },
+        { display: `Q3 ${moment().subtract(1, 'year').year().toString()}`, start: this.getStartOfMonth(6, true), end: this.getEndOfMonth(8, true) },
+        { display: `Q2 ${moment().subtract(1, 'year').year().toString()}`, start: this.getStartOfMonth(3, true), end: this.getEndOfMonth(5, true) },
+        { display: `Q1 ${moment().subtract(1, 'year').year().toString()}`, start: this.getStartOfMonth(0, true), end: this.getEndOfMonth(2, true) }]
 
 
 
 
-  constructor(private dataService: DataService) { }
+    constructor(private dataService: DataService) { }
 
-  ngOnInit(): void {
-    this.dataService.getData().subscribe((response: any) => {
-      // this.chartData = this.dataService.parse(response);
-      this.chartData = response;
-      this.currency = this.chartData.currencySymbol;
-      this.data = this.dataService.parse(this.chartData.data, this.currency);
-      this.columns = ['', ...this.chartData.legend]
+    ngOnInit(): void {
+        this.dataService.getData().subscribe((response: any) => {
+            // this.chartData = this.dataService.parse(response);
+            this.chartData = response;
+            this.currency = this.chartData.currencySymbol;
+            this.data = this.dataService.parse(this.chartData.data, this.currency);
+            this.columns = ['', ...this.chartData.legend]
 
-      // Here is where we want to preserve the data from the backend, and only manipulate the displayData
-      this.displayData = this.dataService.filterByDate(this.data, moment().subtract(30, 'days').toISOString(), moment().toISOString());
-      this.stats = this.dataService.calculateStats(this.displayData);
-  })
-}
+            // Here is where we want to preserve the data from the backend, and only manipulate the displayData
+            this.displayData = this.dataService.filterByDate(this.data, moment().subtract(30, 'days').toISOString(), moment().toISOString());
+            this.stats = this.dataService.calculateStats(this.displayData);
+        })
+    }
 
-// Is this deprecated?
-clearData(){
-  this.data = this.dataService.parse(this.chartData.data, this.currency);
-}
+    // Is this deprecated?
+    clearData() {
+        this.data = this.dataService.parse(this.chartData.data, this.currency);
+    }
 
-getStartOfMonth(month: number, previousYear?: boolean): string{
-  // months will be 0 based indexes from 0-11 so January = 0, December = 11
-  if(previousYear){
-    return moment().subtract(1, 'year').month(month).startOf('month').toISOString()
-  }
-  else{
-    return moment().month(month).startOf('month').toISOString()
-  }
-}
+    getStartOfMonth(month: number, previousYear?: boolean): string {
+        // months will be 0 based indexes from 0-11 so January = 0, December = 11
+        if (previousYear) {
+            return moment().subtract(1, 'year').month(month).startOf('month').toISOString()
+        }
+        else {
+            return moment().month(month).startOf('month').toISOString()
+        }
+    }
 
-getEndOfMonth(month: number, previousYear?: boolean): string{
-  if(previousYear){
-    return moment().subtract(1, 'year').month(month).endOf('month').toISOString()
-  }
-  else{
-    return moment().month(month).endOf('month').toISOString()
-  }
-}
+    getEndOfMonth(month: number, previousYear?: boolean): string {
+        if (previousYear) {
+            return moment().subtract(1, 'year').month(month).endOf('month').toISOString()
+        }
+        else {
+            return moment().month(month).endOf('month').toISOString()
+        }
+    }
 
-dateRangeSelected(idx){
-  let timeRange = this.timeRanges[idx];
-  this.displayData = this.dataService.filterByDate(this.data, timeRange.start, timeRange.end)
-}
+    dateRangeSelected(idx) {
+        let timeRange = this.timeRanges[idx];
+        console.log('time range', timeRange)
+        this.displayData = this.dataService.filterByDate(this.data, timeRange.start, timeRange.end);
+        this.stats = this.stats = this.dataService.calculateStats(this.displayData);
+    }
 
-groupBy(event){
+    groupBy(event: string) {
+        this.intervalToGroupBy = event;
+        this.displayData = this.dataService.groupBy(this.displayData, event);
+    }
 
-}
+    exportAsSpreadsheet() {
+        
+        let element = document.getElementById('excel-table');
 
-onSelect(event){
-  console.log(event)
-}
+        // create the workbook and the sheet
+        const worksheet: XLSX.WorkSheet = XLSX.utils.table_to_sheet(element);
+        const workbook: XLSX.WorkBook = XLSX.utils.book_new();
+
+        XLSX.utils.book_append_sheet(workbook, worksheet, 'Sheet1');
+
+        XLSX.writeFile(workbook, 'Your Report');
+    }
+
+    // This is the select event from the googleChart
+    onSelect(event) {
+
+    }
 
 }
--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,108 +1,149 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { DatePipe } from '@angular/common';
+import * as moment from 'moment';
 
 
 
 export interface IChartData {
-  chartSubTitle: string,
-  chartTitle: string,
-  currencySymbol: string,
-  data: any[]
-  legend: string[]
+    chartSubTitle: string,
+    chartTitle: string,
+    currencySymbol: string,
+    data: any[]
+    legend: string[]
 }
 
 export enum IData {
-  Date = 0,
-  Services = 1,
-  Products = 2,
-  Recurring = 3,
-  Expenses = 4
+    Date = 0,
+    Services = 1,
+    Products = 2,
+    Recurring = 3,
+    Expenses = 4
 }
 
 export interface ITimeRange {
-  display: string,
-  start: string,
-  end: string,
+    display: string,
+    start: string,
+    end: string,
 }
 
 export interface IStats {
-  avgRev: number,
-  mostProfitableDay: {val: number, date: string},
-  mostExpensiveDay: {val: number, date: string},
-  operatingExpenseRatio: number
+    avgRev: number,
+    mostProfitableDay: { val: number, date: string },
+    mostExpensiveDay: { val: number, date: string },
+    operatingExpenseRatio: number
 }
 
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 
 export class DataService {
 
-  constructor(private http: HttpClient, private datePipe: DatePipe) { }
+    constructor(private http: HttpClient, private datePipe: DatePipe) { }
 
-  getData() {
-    // using the cors-anywhere url as a proxy to bypass the cors check by the browser
-    return this.http.get('https://cors-anywhere.herokuapp.com/https://dashboard-qa.ikizmet.com/api/v2/revenues/test');
-  }
+    getData() {
+        // using the cors-anywhere url as a proxy to bypass the cors check by the browser
+        return this.http.get('https://cors-anywhere.herokuapp.com/https://dashboard-qa.ikizmet.com/api/v2/revenues/test');
+    }
 
-  parse(data, currency: string) {
-    return data.map(el => {
-      // let's make our expenses positive for the sake of displaying it to the user on a graph
-      el[1][3] = Math.abs(el[1][3]);
+    parse(data, currency: string) {
+        return data.map(el => {
+            // let's make our expenses positive for the sake of displaying it to the user on a graph
+            el[1][3] = Math.abs(el[1][3]);
 
-      // this creates an object with a numerical value and a display value to be consumed by the google charts
-      // el[1] = el[1].map(num => {
-      //   return { v: num, f: currency + Number(num).toLocaleString() }
-      // });
+            // this creates an object with a numerical value and a display value to be consumed by the google charts
+            // el[1] = el[1].map(num => {
+            //   return { v: num, f: currency + Number(num).toLocaleString() }
+            // });
 
-      return [el[0], ...el[1]]
-    })
-  }
+            return [el[0], ...el[1]]
+        })
+    }
 
-  // Is this deprecated?
-  formatDate(date) {
-    // let formattedDate = this.datePipe.transform(date, 'MMMd');
-    let formattedDate = this.datePipe.transform(date, 'yyyy-MM-ddTHH:mm:ss.SSS');
-    console.log(formattedDate);
-    return formattedDate;
-  }
+    // Is this deprecated?
+    formatDate(date) {
+        // let formattedDate = this.datePipe.transform(date, 'MMMd');
+        let formattedDate = this.datePipe.transform(date, 'yyyy-MM-ddTHH:mm:ss.SSS');
+        return formattedDate;
+    }
 
-  filterByDate(input: any[], start: string, end: string): any[] {
-    return input.filter(data => {
-      let time = data[0]
-      time = this.datePipe.transform(time, 'yyyy-MM-ddTHH:mm:ss.SSS');
-      return time > start && time < end;
-    })
-  }
+    filterByDate(input: any[], start: string, end: string): any[] {
+        return input.filter(data => {
+            let time = data[0]
+            time = this.datePipe.transform(time, 'yyyy-MM-ddTHH:mm:ss.SSS');
+            console.log(this.datePipe.transform('Wed Jan 01 2020 00:00:00 GMT-0800', 'yyyy-MM-ddTHH:mm:ss.SSS'))
+            return time >= start && time <= end;
+        })
+    }
 
-  calculateStats(input: any[][]): IStats {
-    // Let's try to do this in one loop thru the data for performance sake since it is already a nested structure
-    let totalRev = 0;
-    let mostProfitableDay = { val: 0, date: '' }
-    let mostExpensiveDay = { val: 0, date: '' }
-    let totalExpenses = 0;
-      input.forEach(day => {
-        // add up products, services, recurring to get the daily revenue
-        let dailyRev = day[IData.Products] + day[IData.Services] + day[IData.Recurring];
-        // get the daily expenses
-        let dailyExp = day[IData.Expenses];
+    calculateStats(input: any[][]): IStats {
+        // Let's try to do this in one loop thru the data for performance sake since it is already a nested structure
+        let totalRev = 0;
+        let mostProfitableDay = { val: 0, date: '' }
+        let mostExpensiveDay = { val: 0, date: '' }
+        let totalExpenses = 0;
+        input.forEach(day => {
+            // add up products, services, recurring to get the daily revenue
+            let dailyRev = day[IData.Products] + day[IData.Services] + day[IData.Recurring];
+            // get the daily expenses
+            let dailyExp = day[IData.Expenses];
 
-        // Add the current day's revenue to the total for the period, same for expenses
-        totalRev += dailyRev;
-        totalExpenses += dailyExp;
+            // Add the current day's revenue to the total for the period, same for expenses
+            totalRev += dailyRev;
+            totalExpenses += dailyExp;
 
-        // if this daily value is higher than the previous high, it becomes the most profitable day
-        if(dailyRev > mostProfitableDay.val){
-          mostProfitableDay = {val: dailyRev, date: day[IData.Date]}
-        }
-        // do the same thing for expenses
-        if(dailyExp > mostExpensiveDay.val){
-          mostExpensiveDay = {val: dailyExp, date: day[IData.Date]}
-        }
-    })
+            // if this daily value is higher than the previous high, it becomes the most profitable day
+            if (dailyRev > mostProfitableDay.val) {
+                mostProfitableDay = { val: dailyRev, date: day[IData.Date] }
+            }
+            // do the same thing for expenses
+            if (dailyExp > mostExpensiveDay.val) {
+                mostExpensiveDay = { val: dailyExp, date: day[IData.Date] }
+            }
+        })
 
-    return <IStats>{avgRev: totalRev/input.length, mostProfitableDay: mostProfitableDay, mostExpensiveDay: mostExpensiveDay, operatingExpenseRatio: totalExpenses/totalRev}
-  }
+        return <IStats>{ avgRev: totalRev / input.length, mostProfitableDay: mostProfitableDay, mostExpensiveDay: mostExpensiveDay, operatingExpenseRatio: totalExpenses / totalRev }
+    }
+
+    // TODO: set a type for returned display data
+    groupBy(input: any[], period: string): any[] {
+        let result = [[]];
+        console.log(input)
+        // Since the data is already sorted by date, we will keep track of the first instance and get it's month
+        let currMonth = moment(this.formatDate(input[0][IData.Date])).month().toString();
+        let monthCounter = 0
+        input.forEach(day => {
+            if (moment(this.formatDate(day[IData.Date])).month().toString() != currMonth) {
+                // if the month doesn't match, that means we are in a new month and increment the month counter
+                monthCounter++;
+                result[monthCounter] = [];
+                currMonth = moment(this.formatDate(day[IData.Date])).month().toString();
+            }
+            // add the current day to the subarray with the other's from it's month
+            result[monthCounter].push(day);
+        })
+        return this.reduceDays(result);
+    }
+
+    reduceDays(input: any[]): any[] {
+        let result = [];
+        
+        input.forEach(interval => {
+            interval.reduce((prevDay, currDay) => {
+                prevDay[IData.Expenses] += currDay[IData.Expenses]
+                prevDay[IData.Recurring] += currDay[IData.Recurring]
+                prevDay[IData.Products] += currDay[IData.Products]
+                prevDay[IData.Services] += currDay[IData.Services]
+                return prevDay;
+            })
+            // once we have reduced the value we can reassign the interval
+            console.log('first interval', interval[0])
+            result.push(interval[0]);
+        })
+
+        console.log(result);
+        return result;
+    }
 }
